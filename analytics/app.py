231070b4ba784047a1dc6c4ec5be1fd706ddaae3
MMShed <88801787+MMShed@users.noreply.github.com>
2026-07-15 15:17:38 -0400

    Fix CI failures: remove unused flask.abort import, drop missing npm lockfile cache

--- analytics/app.py
+++ analytics/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 from analytics_service import AnalyticsService
